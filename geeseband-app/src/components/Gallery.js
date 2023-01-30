386c25c5d00bd809ac113967840efb5b9c889454
Andrew Bogaard <abogaard@gmail.com>
2023-01-30 23:01:28 +0100

    new site and workflow attempt

--- geeseband-app/src/components/Gallery.js
+++ geeseband-app/src/components/Gallery.js
@@ -1,18 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import Images from './ImageArray'
-import Frame from '../assets/wooden_frame.png'
+import img_src from './ImageArray';
+import Frame from '../assets/frame_shadow_comp.png';
+import strftime from 'strftime'
+
 var poissonProcess = require('poisson-process');
 
 
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 const Gallery = (props) => {
-  const [display, setDisplay] = React.useState(Images[0])
+  const [display, setDisplay] = React.useState(img_src[0])
 
   useEffect(() => {
     const p = poissonProcess.create(5000, function updateImage() {
-      var r = Math.floor(Math.random() * 23);
-      if (Images[r]) {
-        setDisplay(Images[r])
+      console.log(img_src.length);
+      var r = Math.floor(Math.random() * img_src.length);
+      // console.log(getRandomInt(2023010100, strftime('%Y%m%d%H', new Date())));
+      if (img_src[r]) {
+        setDisplay(img_src[r])
       }
       
     })
--- geeseband-app/src/index.js
+++ geeseband-app/src/index.js
@@ -2,25 +2,26 @@ import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Gallery from './components/Gallery.js'
+import {ReactComponent as Twiddle} from './assets/twiddle.svg'
 
 class Page extends React.Component {
 
   render() {
     return (
       <div className="main_div">
-        <div className="center_div">
-          <div className ='title_div'>GEESE</div>
-          <div className="gallery">
-           <Gallery/>
-          </div>
-          <div className = 'nav_div'>
-            <ul className = 'nav_ul'>
-              <li><button className="link"> Cowboy Nudes </button></li>
-              <li><a href="https://www.youtube.com/@Geeseband" className= "link">Watch</a></li>
-              <li><a href="https://www.bandsintown.com/a/342405-geese" className="link" >Tour</a></li>
-              <li><a href="https://store.geeseband.com/" className="link" >Merch</a></li>
-              <li><button className="link">Newsletter</button></li>
-            </ul>
+        <div className="gallery">
+         <Gallery/>
+        </div>
+        <div className = 'nav_div'>
+          <div className = 'tag_div'>
+            <div className ='title_div'>GEESE</div>
+            <Twiddle style={{height:'12px', width:'auto'}}/>
+              <a href="https://geese.lnk.to/cowboynudes" target="_blank" className= "link"> cowboy nudes </a>
+              <a href="https://www.youtube.com/watch?v=LvHsLdYXfaY" target="_blank" className= "link">watch</a>
+              <a href="https://www.bandsintown.com/a/342405-geese" target="_blank" className="link" >tour</a>
+              <a href="https://store.geeseband.com/" target="_blank" className="link" >merch</a>
+              <a href="http://eepurl.com/hkwcxv" target="_blank" className= "link">newsletter</a>
+
           </div>
         </div>
       </div>
